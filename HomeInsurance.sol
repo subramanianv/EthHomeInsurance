0f7f8617b5f62be7e5801d43413b128ddf4edd6e
Subramanian <subramanian@gmail.com>
2016-07-23 21:22:45 -0400

    Home Insurance latest

--- HomeInsurance.sol
+++ HomeInsurance.sol
@@ -5,21 +5,16 @@ contract HomeInsurance{
     uint PAYOUT = 1;
     uint costToJoinInEther = 1;
     mapping (address => bool) members;
+    mapping (uint => Claim) claims;
+    uint claimId = 0;
     uint total_amount;
-    Claim current_claim;
-    Claim c;
 
-
-    struct member{
+    struct member {
         address member_address;
+        uint premium;
+        uint lastPremium;
     }
-
-    // struct Vote{
-    //     bool approved;
-    //     address voter;
-    // }
-
-    struct Claim{
+    struct Claim {
         address claimant;
         string description;
         mapping (address => bool) voters;
@@ -27,6 +22,8 @@ contract HomeInsurance{
         uint noVotes;
         uint totalVotes;
         bool processed;
+        uint payout;
+        uint claimId;
     }
 
     function HomeInsurance () {
@@ -47,11 +44,6 @@ contract HomeInsurance{
             total_amount += msg_value;
         }
     }
-    function getClaim() constant returns(string){
-        return current_claim.description;
-
-    }
-
 
     function isMember(address input) constant returns(bool){
         if(members[input] == true){
@@ -60,62 +52,70 @@ contract HomeInsurance{
             return false;
         }
     }
-
-    function amIMember() constant returns (bool){
-        address msg_sender = msg.sender;
-        if(members[msg_sender] == true){
-            return true;
-        } else{
-            return false;
-        }
+    function getDescription(uint _claimId) constant returns(string) {
+         return claims[_claimId].description;
     }
 
-    function makeClaim (string desc){
+    function makeClaim (uint claimAmount, string desc){
         address msg_sender = msg.sender;
         if(members[msg_sender]  != true){
             return;
         }else {
+            uint c_id= ++claimId;
+            Claim c = claims[c_id];
+            c.claimId = c_id;
             c.claimant = msg_sender;
             c.description = desc;
             c.processed = false;
-            current_claim = c;
+            c.payout = claimAmount;
+            c.yesVotes = 0;
+            c.noVotes = 0;
+            c.totalVotes = 0;
         }
     }
-    function resolveVotesAndPayout(){
-       if(current_claim.totalVotes >= VOTING_THRESHOLD  && current_claim.yesVotes >= current_claim.noVotes) {
-            current_claim.claimant.send(PAYOUT);
-        }
 
-
+    function currentClaimId() constant returns (uint) {
+      return claimId;
     }
-
-
-    function castVote (bool approve){
+    function castVote (uint _claimId, bool approve) {
         address msg_sender = msg.sender;
-        if(current_claim.claimant == msg_sender) {
+
+        if(claims[_claimId].processed == true) {
+          return;
+        }
+        if(claims[_claimId].claimant == msg_sender) {
             return;
         }
-        if(current_claim.voters[msg_sender] == true) {
+        if(claims[_claimId].voters[msg_sender] == true) {
             return;
         }
-        current_claim.voters[msg_sender] = true;
+        claims[_claimId].voters[msg_sender] = true;
         if(members[msg_sender] == true){
             if(approve == true) {
-                current_claim.yesVotes +=1;
+                claims[_claimId].yesVotes +=1;
             }else{
-                current_claim.noVotes +=1;
+                claims[_claimId].noVotes +=1;
             }
-            current_claim.totalVotes +=1;
+            claims[_claimId].totalVotes +=1;
         } else{
             return;
         }
-        resolveVotesAndPayout();
 
+        if(claims[_claimId].totalVotes >= VOTING_THRESHOLD  &&
+          claims[_claimId].yesVotes >= claims[_claimId].noVotes)
+           {
+            claims[_claimId].processed = true;
+            uint _p = claims[_claimId].payout * 1000000000000000000;
+            claims[_claimId].claimant.send(_p);
+        }
     }
 
-    function yesCount () constant returns (uint){
-        uint count = 0;
-        return current_claim.yesVotes;
+    function yesCount (uint _claimId) constant returns (uint){
+        return claims[_claimId].yesVotes;
+    }
+
+    function noCounts (uint _claimId) constant returns (uint) {
+      return claims[_claimId].noVotes;
     }
 
 }
